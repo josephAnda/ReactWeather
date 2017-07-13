009739455338c9d75aab2a4e2a98804b476d5799
josephAnda <orenmurasaki@gmail.com>
2017-07-12 21:29:29 -0700

    redirected http

--- server.js
+++ server.js
@@ -5,13 +5,20 @@ var express = require('express');
 //  Create our app
 var app = express();
 //  the port is accessed this way on behalf of heroku 
-var port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
+app.use(function (req, res, next) {
+	if (req.headers['x-forwarded-proto'] === 'http') {
+		next();
+	} else {
+		res.redirect('http://' + req.hostname + req.url);
+	}
+});
 //  app.use() tells what folder we want to serve
 //  express.static() gives the folder name that we want to expose
 app.use(express.static('public')); 
 
 //  app.listen() starts the server
 app.listen(port, function() {
-	console.log('Express server is up on port' + port);
+	console.log('Express server is up on port' + PORT);
 });
