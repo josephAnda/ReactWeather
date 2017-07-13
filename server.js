2234368b92c6129781fe0fd32247ba6bc77db672
josephAnda <orenmurasaki@gmail.com>
2017-07-12 21:24:52 -0700

    starting server via heroku

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ var express = require('express');
 
 //  Create our app
 var app = express();
+//  the port is accessed this way on behalf of heroku 
+var port = process.env.PORT || 3000;
 
 //  app.use() tells what folder we want to serve
 //  express.static() gives the folder name that we want to expose
 app.use(express.static('public')); 
 
 //  app.listen() starts the server
-app.listen(3000, function() {
-	console.log('Express server is up on port 3000');
+app.listen(port, function() {
+	console.log('Express server is up on port' + port);
 });
