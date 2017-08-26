a1d51fc9d4e0289bec1cccff3dd051362f89fc91
josephAnda <orenmurasaki@gmail.com>
2017-08-26 13:48:13 -0700

    Prepped repo for heroku deployment

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-//  The express dependency was installed using npm 
+//  The express dependency was installed using npm
 
 var express = require('express');
 
 //  Create our app
 var app = express();
-
+const PORT = process.env.PORT || 3000;
 //  app.use() tells what folder we want to serve
 //  express.static() gives the folder name that we want to expose
-app.use(express.static('public')); 
+app.use(function(req, res, next ) {
+	if (req.headers['x-forwarded-proto'] === 'http') {
+		next();
+	} else {
+		res.redirect('http://' + req.hostname + req.url);
+	}
+});
+
+app.use(express.static('public'));
 
 //  app.listen() starts the server
-app.listen(3000, function() {
-	console.log('Express server is up on port 3000');
+app.listen(PORT, function() {
+	console.log('Express server is up on port ' + port);
 });
