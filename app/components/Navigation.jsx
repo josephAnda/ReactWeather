3f0d01b647078749c2576f13d3d96625f8e42649
josephAnda <orenmurasaki@gmail.com>
2017-08-31 14:29:26 -0700

    added search bar and changed navigation from stateless functional comp

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -4,30 +4,43 @@ var {Link, IndexLink} = require('react-router');
 //  as an IndexRoute in app.jsx
 
 //  Use className instead of class in JSX html tags to prevent js conflict
-var Navigation = () => {
-	return (
-		<div className="top-bar">
-			<div className="top-bar-left">
-				<ul className="menu">
-					<li className="menu-text">React Weather App</li>
-					<li>
-						<IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-					</li>
-					<li>
-						<IndexLink to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</IndexLink>
-					</li>
-					<li>
-						<IndexLink to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</IndexLink>
-					</li>
-				</ul>
+var Navigation = React.createClass({
+	onSearch: function(e) {
+		e.preventDefault();
+		alert('Not yet wired up!');
+	},
+	render: function() {
+		return (
+			<div className="top-bar">
+				<div className="top-bar-left">
+					<ul className="menu">
+						<li className="menu-text">React Weather App</li>
+						<li>
+							<IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+						</li>
+						<li>
+							<IndexLink to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</IndexLink>
+						</li>
+						<li>
+							<IndexLink to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</IndexLink>
+						</li>
+					</ul>
+				</div>
+				<div className="top-bar-right">
+					<form onSubmit={this.onSearch}>
+						<ul className="menu">
+							<li>
+								<input type="search" placeholder="Search weather"/>
+							</li>
+							<li>
+								<input type="submit" className="button" value="Get Weather" />
+							</li>
+						</ul>
+					</form>
+				</div>
 			</div>
-			<div className="top-bar-right">
-
-			</div>
-
-		</div>
-
-	);
-};
+		);
+	}
+});
 
 module.exports = Navigation;
