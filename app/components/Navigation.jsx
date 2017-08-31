fd6de6568add567a51e230b8cad9d5e5e12ff079
josephAnda <orenmurasaki@gmail.com>
2017-08-31 14:13:34 -0700

    added basic navigation styling to navigation.jsx

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -2,14 +2,31 @@ var React = require('react');
 var {Link, IndexLink} = require('react-router');
 //  IndexLink is preferred if the component is going to be referenced
 //  as an IndexRoute in app.jsx
+
+//  Use className instead of class in JSX html tags to prevent js conflict
 var Navigation = () => {
 	return (
-		<div>
-			<h2>Navigation Component</h2>
-			<IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-			<IndexLink to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</IndexLink>
-			<IndexLink to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</IndexLink>
+		<div className="top-bar">
+			<div className="top-bar-left">
+				<ul className="menu">
+					<li className="menu-text">React Weather App</li>
+					<li>
+						<IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+					</li>
+					<li>
+						<IndexLink to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</IndexLink>
+					</li>
+					<li>
+						<IndexLink to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</IndexLink>
+					</li>
+				</ul>
+			</div>
+			<div className="top-bar-right">
+
+			</div>
+
 		</div>
+
 	);
 };
 
