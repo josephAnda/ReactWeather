b3b8ee764433edc731e14e7f8df67cf209031a2c
josephAnda <orenmurasaki@gmail.com>
2017-09-02 14:26:06 -0700

    added ordered list to examples page with new Foundation styles

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,12 +1,15 @@
 var React = require('react');
 var Navigation = require('Navigation');
-//  Note that 'props' rather than 'this.props' will be used if you're dealing with arrow functions 
+//  Note that 'props' rather than 'this.props' will be used if you're dealing with arrow functions
 var Main = (props) => {
 	return (
 		<div>
 			<Navigation />
-			<h2>Main Component</h2>
-			{props.children} 
+			<div className="row">
+				<div className="columns medium-6 large-4 small-centered">
+					{props.children}
+				</div>
+			</div>
 		</div>
 	);
 };
