b7ce19cd9b845565c34f07315a9c97958662f825
josephAnda <orenmurasaki@gmail.com>
2017-09-02 15:30:39 -0700

    updated the about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -14,7 +14,19 @@ var About = () => {
 				<li>Experiment with styles and structuring tools from Foundation.</li>
 				<li>Serve as a testing ground for implementing various design strategies.</li>
 			</ul>
-			<p>Check out <a href="https://github.com/josephAnda" target="_blank">my github profile</a> for more projects.</p>
+			<p>Here are some of the tools that I used:</p>
+			<ul>
+				<li>
+					<a href="https://facebook.github.io/react/">React</a>
+				</li>
+				<li>
+					<a href="https://babeljs.io/">Babel</a>
+				</li>
+				<li>
+					<a href="http://foundation.zurb.com/">Foundation</a>
+				</li>
+			</ul>
+			<p>Check out <a href="https://github.com/josephAnda">my github profile</a> for more projects.</p>
 		</div>
 	)
 };
